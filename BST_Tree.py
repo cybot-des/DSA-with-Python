ed33152b652249fa25d9aaca21b95d5e8fd3dec5
Pooja S Deshpande <44392532+cybot-des@users.noreply.github.com>
2021-12-03 23:51:51 +0530

    Add files via upload

---
+++ BST_Tree.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data)
+        if self.right:
+            self.right.print_tree()
+
+
+root = Node(25)
+root.insert(78)
+root.insert(43)
+root.insert(100)
+root.insert(54)
+root.insert(12)
+root.insert(0)
+root.insert(167)
+
+root.print_tree()
